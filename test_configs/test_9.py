cb5d9d2f7ff486a16132b3f20a671a45440f494a
SamuelMallick <sam.mallick.97@gmail.com>
2024-02-13 15:21:29 +0100

    Some config files

---
+++ test_configs/test_9.py
@@ -0,0 +1,48 @@
+from typing import Literal
+
+import numpy as np
+from mpcrl import ExperienceReplay, UpdateStrategy, optim
+
+# Second-order
+
+
+class Test:
+    # simulation and training params
+    test_ID = "9"
+    num_days = 40
+    ep_len = num_days * 24 * 4  # 'x' days of 15 minute timesteps
+    num_episodes = 100
+
+    # mpc and model params
+    base_model: Literal[
+        "nonlinear", "rk4", "euler"
+    ] = "rk4"  # underlying simulation model
+    prediction_model = "rk4"  # mpc prediction model
+    horizon = 24
+    discount_factor = 0.99
+    rl_cost = {"c_u": [0, 0, 0], "c_y": 0, "c_dy": 100, "w": 0 * np.ones((1, 4))}
+
+    # learning params
+    learn_all_p = True  # if false we only learn the sensitive subset of p
+    # fixed pars and learable pars
+    learnable_pars_init = {
+        "V0": np.zeros((1,)),
+        "c_dy": 100 * np.ones((1,)),
+    }
+    fixed_pars = {
+        "c_u": np.array([0, 0, 0]),
+        "c_y": 0 * np.ones((1,)),
+        "w": 1e3 * np.ones((1, 4)),
+    }
+
+    update_strategy = UpdateStrategy(frequency=1, hook="on_episode_end")
+    learning_rate = 1e-3
+    optimizer = optim.NetwonMethod(learning_rate=learning_rate)
+    exploration = None
+    experience = ExperienceReplay(
+        maxlen=3 * ep_len,
+        sample_size=2 * ep_len,
+        include_latest=ep_len,
+        seed=0,
+    )
+    hessian_type = "approx"
