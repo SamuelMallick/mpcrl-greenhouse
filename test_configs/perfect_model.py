d3e3525fd8be302b5ff662fb2a414d9c0def86be
mallick <sam.mallick.97@gmail.com>
2024-03-04 13:37:37 +0100

    test configs

--- test_configs/perfect_model.py
+++ test_configs/perfect_model.py
@@ -7,10 +7,10 @@ from mpcrl.core.schedulers import ExponentialScheduler
 
 class Test:
     # simulation and training params
-    test_ID = "default"
-    num_days = 2
+    test_ID = "perf_model"
+    num_days = 40
     ep_len = num_days * 24 * 4  # 'x' days of 15 minute timesteps
-    num_episodes = 2
+    num_episodes = 50
 
     # mpc and model params
     base_model: Literal[
@@ -19,37 +19,35 @@ class Test:
     prediction_model = "rk4"  # mpc prediction model
     horizon = 24
     discount_factor = 0.99
-    rl_cost = {"c_u": [10, 1, 1], "c_y": 1e3, "c_dy": 100, "w": 1e3 * np.ones((1, 4))}
-    p_perturb = [0]  # index of parameters that are perturbed
+    rl_cost = {"c_u": [0, 0, 0], "c_y": 0, "c_dy": 100, "w": 0 * np.ones((1, 4))}
+    p_perturb: list = []  # index of parameters that are perturbed
 
     # learning params
-    p_learn = [0, 15]  # index of parameters to learn
+    p_learn = [i for i in range(28)]  # index of parameters to learn
     # fixed pars and learable pars
     learnable_pars_init = {
         "V0": np.zeros((1,)),
-        "c_u": np.array([10, 1, 1]),
-        "c_y": 1e3 * np.ones((1,)),
         "c_dy": 100 * np.ones((1,)),
-        "w": 1e3 * np.ones((1, 4)),
     }
     # bounds on learnable pars
     learn_bounds = {
-        "c_u": [0, np.inf],
-        "c_y": [0, np.inf],
         "c_dy": [0, np.inf],
-        "w": [0, np.inf],
     }
-    fixed_pars = {}
+    fixed_pars = {
+        "c_u": np.array([0, 0, 0]),
+        "c_y": 1e3 * np.ones((1,)),
+        "w": 1e3 * np.ones((1, 4)),
+    }
 
-    update_strategy = UpdateStrategy(int(ep_len / 2), skip_first=2)
-    learning_rate = 1e-3
+    update_strategy = UpdateStrategy(1, hook="on_episode_end")
+    learning_rate = 1e-2
     optimizer = optim.NetwonMethod(
         learning_rate=ExponentialScheduler(learning_rate, factor=1)
     )
     exploration = None
     experience = ExperienceReplay(
-        maxlen=10 * ep_len,
-        sample_size=3 * ep_len,
+        maxlen=3 * ep_len,
+        sample_size=2 * ep_len,
         include_latest=ep_len,
         seed=0,
     )
