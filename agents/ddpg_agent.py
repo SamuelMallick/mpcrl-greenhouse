23cee4ce2e9ee1387f30bee57f366d7ccee3aa24
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-05-31 19:44:39 +0200

    implemented DDPG

---
+++ agents/ddpg_agent.py
@@ -0,0 +1,174 @@
+from functools import partial
+from itertools import chain
+from operator import neg
+
+import numpy as np
+import torch as th
+from gymnasium import ObservationWrapper, spaces
+from gymnasium.wrappers import TimeLimit, TransformReward
+from mpcrl.wrappers.envs import MonitorEpisodes
+from stable_baselines3 import DDPG
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.noise import OrnsteinUhlenbeckActionNoise
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+
+from greenhouse.env import LettuceGreenHouse
+from greenhouse.model import Model
+
+
+class AugmentedObservationWrapper(ObservationWrapper):
+    """Wrapper that augments the observation with the one-step yield improvement,
+    weather disturbances and previous input."""
+
+    def __init__(self, env: LettuceGreenHouse) -> None:
+        super().__init__(env)
+        nx, nd = env.get_wrapper_attr("nx"), env.get_wrapper_attr("nd")
+        act = env.action_space
+        lby, uby = [-np.inf, 0.0, -273.15, 0.0], np.full(nx, np.inf)
+        lbd, ubd = [0.0, 0.0, -273.15, 0.0], np.full(nd, np.inf)
+        self.observation_space = spaces.Box(
+            np.concatenate((lby, lbd, act.low)),
+            np.concatenate((uby, ubd, act.high)),
+            dtype=act.dtype,
+            seed=act.np_random,
+        )
+
+    def observation(self, state: np.ndarray) -> np.ndarray:
+        env: LettuceGreenHouse = self.env.unwrapped
+        output = Model.output(state, env.p)
+        output[0] -= env.previous_lettuce_yield
+        new_state = np.concatenate(
+            (output, env.current_disturbance, env.previous_action), axis=None
+        )
+        assert self.observation_space.contains(new_state), "Invalid observation."
+        return new_state
+
+
+def clip_grad(grad: th.Tensor, max_norm: float) -> th.Tensor:
+    """Clips the gradient's L2 norm to a maximum value."""
+    norm = th.linalg.vector_norm(grad)
+    clip_coef = th.clamp_max(max_norm / (norm + 1e-6), 1.0)
+    return grad.mul(clip_coef)
+
+
+def make_env(
+    gamma: float, days: int, evaluation: bool = False, seed: int | None = None
+) -> tuple[VecNormalize, int]:
+    """Creates and appropriately wraps the greenhouse env for training or evaluation,
+    and returns also the number of steps per episode."""
+    greenhouse = LettuceGreenHouse(
+        growing_days=days,
+        model_type="continuous",
+        disturbance_type="multiple",
+        testing=evaluation,
+    )
+    max_episode_steps = days * LettuceGreenHouse.steps_per_day
+    env = MonitorEpisodes(TimeLimit(greenhouse, max_episode_steps=max_episode_steps))
+
+    # make the env compatible with RL - augment state, and flip cost into reward
+    env = TransformReward(AugmentedObservationWrapper(env), neg)
+
+    # add wrappers for SB3
+    env = Monitor(env)
+    venv = DummyVecEnv([lambda: env])
+    venv = VecNormalize(
+        venv, not evaluation, clip_obs=np.inf, clip_reward=np.inf, gamma=gamma
+    )
+    venv.seed(seed)
+    return venv, min(max_episode_steps, greenhouse.yield_step)
+
+
+def train_ddpg(
+    episodes: int,
+    days_per_episode: int,
+    learning_rate: float,
+    gradient_threshold: float,
+    l2_regularization: float,
+    batch_size: int,
+    buffer_size: int,
+    gamma: float,
+    seed: int,
+    device: str,
+    verbose: int,
+) -> tuple[MonitorEpisodes, MonitorEpisodes]:
+    """Trains a DDPG agent on the greenhouse environment.
+
+    Parameters
+    ----------
+    episodes : int
+        Number of episodes to train the agent for.
+    days_per_episode : int
+        Number of days per episode to simualte.
+    learning_rate : float
+        The learning rate of the RL algorithm.
+    gradient_threshold : float
+        Threshold for the gradient norm clipping.
+    l2_regularization : float
+        L2 regularization strength, i.e., rate of weight decay.
+    batch_size : int
+        Size of mini-batches sampled from the replay buffer when updating.
+    buffer_size : int
+        Size of the whole replay buffer.
+    gamma : float
+        Discount factor.
+    seed : int
+        RNG seed for the simulation.
+    device : "auto", or "cpu", or "cuda:0", etc.
+        Device to use for training.
+    verbose : {0, 1, 2, 3}
+        Level of verbosity of the agent's logger.
+
+    Returns
+    -------
+    tuple of two envs wrapped in `MonitorEpisodes`
+        Returns the training and evaluation environments wrapped in `MonitorEpisodes`.
+    """
+    set_random_seed(seed, using_cuda=device.startswith("cuda"))
+
+    # create the model
+    env, steps_per_episode = make_env(gamma, days_per_episode, seed=seed)
+    na = env.action_space.shape[-1]
+    action_noise = OrnsteinUhlenbeckActionNoise(np.zeros(na), np.full(na, 0.3), dt=1.0)
+    model = DDPG(
+        "MlpPolicy",
+        env,
+        learning_rate=learning_rate,
+        buffer_size=buffer_size,
+        batch_size=batch_size,
+        learning_starts=batch_size,
+        tau=1e-3,
+        gamma=gamma,
+        train_freq=(96, "step"),
+        gradient_steps=-1,
+        action_noise=action_noise,
+        policy_kwargs={
+            "net_arch": [256, 256],
+            "optimizer_kwargs": {"weight_decay": l2_regularization},
+        },
+        verbose=verbose,
+        seed=seed,
+        device=device,
+    )
+
+    # add hooks to perform L2 gradient norm clipping
+    for p in chain(model.actor.parameters(), model.critic.parameters()):
+        p.register_hook(partial(clip_grad, max_norm=gradient_threshold))
+
+    # create the eval callback
+    eval_env, _ = make_env(gamma, days_per_episode, evaluation=True, seed=seed)
+    cb = EvalCallback(
+        eval_env=eval_env,
+        n_eval_episodes=1,
+        eval_freq=steps_per_episode,
+        verbose=verbose,
+    )
+
+    # launch the training
+    total_timesteps = steps_per_episode * episodes
+    model.learn(total_timesteps=total_timesteps, log_interval=1, callback=cb)
+
+    # return as data the `MonitorEpisodes` from the training and evaluation envs - ugly,
+    # but they must be digged out from the `VecNormalize` wrapper
+    return model.get_env().envs[0].env.env.env, eval_env.envs[0].env.env.env
---
+++ train_ddpg.py
@@ -0,0 +1,102 @@
+import pickle
+from typing import Collection, Iterator, Literal, Union
+
+import numpy as np
+from joblib import Parallel, delayed
+from mpcrl.wrappers.envs import MonitorEpisodes
+
+from agents.ddpg_agent import train_ddpg
+from utils.plot import plot_greenhouse
+
+
+def do_training(
+    episodes: int,
+    days_per_episode: int,
+    n_agents: int = 1,
+    seed: int | None = None,
+    devices: Union[str, Collection[str]] = "auto",
+) -> Iterator[tuple[MonitorEpisodes, MonitorEpisodes]]:
+    """Launches the training of `n_agents` DDPG agents in parallel."""
+    if isinstance(devices, str):
+        devices = (devices,)
+    seeds = np.random.SeedSequence(seed).generate_state(n_agents)
+
+    def fun(n: int):
+        return train_ddpg(
+            episodes=episodes,
+            days_per_episode=days_per_episode,
+            learning_rate=1e-3,
+            gradient_threshold=1.0,
+            l2_regularization=1e-5,
+            batch_size=64,
+            buffer_size=10_000,
+            gamma=0.9,
+            seed=int(seeds[n]),
+            device=devices[n % len(devices)],
+            verbose=0,
+        )
+
+    return Parallel(n_jobs=n_agents, return_as="generator_unordered")(
+        delayed(fun)(i) for i in range(n_agents)
+    )
+
+
+def process_simulations(
+    sims: Iterator[tuple[MonitorEpisodes, MonitorEpisodes]]
+) -> dict[str, dict[str, np.ndarray]]:
+    """Extracts the simulations' data into numpy arrays."""
+    train_envs, eval_envs = zip(*sims)
+    data = {}
+    for env_type, envs in [("train", train_envs), ("eval", eval_envs)]:
+        X = np.asarray([env.observations for env in envs])  # agents x ep x T x nx
+        U = np.asarray([env.actions for env in envs])  # agents x ep x T x nu
+        R = np.asarray([env.rewards for env in envs])  # agents x ep x T
+        D = [env.get_wrapper_attr("disturbance_profiles_all_episodes") for env in envs]
+        D = np.asarray(D).swapaxes(-1, -2)  # agents x ep x T x nd
+        data[env_type] = {"X": X, "U": U, "R": R, "D": D}
+    return data
+
+
+def do_plotting(
+    data: dict[str, dict[str, np.ndarray]],
+    agent_to_plot: int,
+    env_type: Literal["train", "eval"],
+) -> None:
+    """Plots the greenhouse state of the agent `agent_to_plot` in the environment
+    `env_type`."""
+    i = agent_to_plot
+    d = data[env_type]
+    plot_greenhouse(d["X"][i], d["U"][i], d["D"][i], d["R"][i], None)
+
+
+def store_data(
+    data: dict[str, dict[str, np.ndarray]], identifier: str = "ddpg"
+) -> None:
+    """Stores the simulation data to disk."""
+    for env_type in ("train", "eval"):
+        X = data[env_type]["X"]
+        U = data[env_type]["U"]
+        D = data[env_type]["D"]
+        R = data[env_type]["R"]
+        for i in range(X.shape[0]):
+            with open(f"{identifier}_{env_type}_{i}.pkl", "wb") as file:
+                pickle.dump({"X": X[i], "U": U[i], "D": D[i], "R": R[i]}, file)
+
+
+if __name__ == "__main__":
+    # launch training
+    simulations = do_training(episodes=1, days_per_episode=2, n_agents=3, seed=1)
+
+    # process and plot or store data
+    PLOT = False
+    STORE_DATA = True
+    if PLOT or STORE_DATA:
+        simdata = process_simulations(simulations)
+
+        if PLOT:
+            # for now, can only plot one agent from one type
+            do_plotting(data=simdata, agent_to_plot=0, env_type="train")
+
+        if STORE_DATA:
+            # for now, each agent from each type is saved to a separate file
+            store_data(simdata)
