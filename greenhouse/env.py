fixed bug with violation normalization

greenhouse/env.py
@@ -107,10 +107,10 @@ class LettuceGreenHouse(gym.Env[npt.NDArray[np.floating], npt.NDArray[np.floatin
             "c_dy", 100.0
         )  # reward on step-wise lettuce yield
         self.w_y = cost_parameters_dict.get(
-            "w_y", np.full(self.nx, 1e4)
+            "w_y", np.full(self.nx, 1e5)
         )  # penatly on constraint violations
         self.w_du = cost_parameters_dict.get(
-            "w_du", np.full(self.nu, 1e3)
+            "w_du", np.full(self.nu, 1e5)
         )  # penatly on control variation constraint violations
 
         self.yield_step = (

sample_greenhouse.py
@@ -44,7 +44,7 @@ sample_mpc = SampleBasedMpc(
     cost_parameters_dict={
         "c_u": np.array([10, 1, 1]),
         "c_y": 1e3,
-        "w_y": 1e4 * np.ones(4),
+        "w_y": 1e5 * np.ones(4),
     },  # MPC cost tuned from 2022 paper
     prediction_model=prediction_model,
     multistarts=multistarts,