ce70df65205b9c03b5bfc4035f0c4079d36a88c8
mallick <sam.mallick.97@gmail.com>
2024-09-17 15:35:05 +0200

    updated bar plotting

--- utils/paper_vizualizations/bar_plots.py
+++ utils/paper_vizualizations/bar_plots.py
@@ -34,8 +34,15 @@ mpc_rl_files = [
     "results/test_93_eval_final.pkl",
     "results/test_80_eval_final.pkl",
 ]
+# ddpg_files = [
+#     "results/ddpg_agent_0_eval_learned.pkl",
+#     "results/ddpg_agent_1_eval_learned.pkl",
+#     "results/ddpg_agent_2_eval_learned.pkl",
+# ]
 ddpg_files = [
     "results/ddpg_eval_0.pkl",
+    "results/ddpg_eval_1.pkl",
+    "results/ddpg_eval_2.pkl",
 ]
 
 p = Model.get_true_parameters()
@@ -61,13 +68,25 @@ for file_name in mpc_rl_files:
     ) as file:
         # stack the data from the different seeds
         mpc_rl_data.append(pickle.load(file))
-data.append({key: np.concatenate([o[key] for o in mpc_rl_data]) for key in ["X", "U", "R", "d"]})
+data.append(
+    {key: np.concatenate([o[key] for o in mpc_rl_data]) for key in ["X", "U", "R", "d"]}
+)
 
 # ddpg
-with open("results/ddpg_eval_0.pkl", "rb") as file:
-    data_ = pickle.load(file)
-    ddpg_data = {key: val[-20:] for key, val in data_.items() if isinstance(val, np.ndarray)}
-    data.append(ddpg_data)
+ddpg_data = []
+for file_name in ddpg_files:
+    with open(file_name, "rb") as file:
+        data_ = pickle.load(file)
+        ddpg_ = {
+            key: val[:20] for key, val in data_.items() if isinstance(val, np.ndarray)
+        }
+        ddpg_data.append(ddpg_)
+data.append(
+    {key: np.concatenate([o[key] for o in ddpg_data]) for key in ["X", "U", "R", "d"]}
+)
+
+labels = ["I-MPC", "N-MPC", "R-MPC-5", "R-MPC-10", "R-MPC-20", "our", "DDPG"]
+colors = ["C0"] + ["C1"] + ["C2"] * 3 + ["C3"] + ["C4"]
 
 _, ep_axs = plt.subplots(
     4, 1, constrained_layout=True, sharex=True
@@ -77,10 +96,14 @@ VIOL_indx = 1
 YIELD_indx = 2
 EPI_indx = 3
 # plot environment rewards
-R = [o["R"] for o in data]
-ep_axs[R_indx].boxplot(
-    [np.sum(o, axis=1) for o in R],
-)  # plot the total reward for each episode
+R = [np.sum(o["R"], axis=1) for o in data]
+mean_R, std_R = zip(
+    *((np.mean(r), np.std(r)) for r in R)
+)
+ep_axs[R_indx].bar(labels, mean_R, color=colors)
+ep_axs[R_indx].errorbar(
+    labels, mean_R, std_R, marker=None, ls="none", color="black", capsize=7
+)
 ep_axs[R_indx].set_yscale("log")
 ep_axs[R_indx].set_ylabel(r"$L_{ep}$")
 
@@ -90,25 +113,37 @@ d_full = [o["d"] for o in data]
 U_full = [o["U"] for o in data]
 viols_full = []
 y_full = []
-for X, d, U in zip(X_full, d_full, U_full): # TODO make like viz
+for X, d, U in zip(X_full, d_full, U_full):
     # generate output data from state data X
     viols, y, _, _ = get_constraint_violations(X, U, d)
     viols_full.append(viols)
     y_full.append(y)
 
 # plot constraint violations
-ep_axs[VIOL_indx].boxplot(
-    [np.sum(viols_full[i].reshape(viols_full[i].shape[0], -1), axis=1) for i in range(len(data))]
-)  # plot the total reward for each episode
-# ep_axs[VIOL_indx].set_yscale("log")
+tot_viols_full = [np.sum(viols, axis=1) for viols in viols_full]
+mean_viols, std_viols = zip(
+    *((np.mean(viols), np.std(viols)) for viols in tot_viols_full)
+)
+ep_axs[VIOL_indx].bar(labels, mean_viols, color=colors)
+ep_axs[VIOL_indx].errorbar(
+    labels, mean_viols, std_viols, marker=None, ls="none", color="black", capsize=7
+)
+ep_axs[VIOL_indx].set_yscale("log")
+ep_axs[VIOL_indx].set_ylim(-10, ep_axs[VIOL_indx].get_ylim()[1])
 ep_axs[VIOL_indx].set_ylabel(r"$\Psi$")
 
 # plot yields
-ep_axs[YIELD_indx].boxplot(
-    [y_full[i][:, -1, 0] for i in range(len(data))],
-)  # plot the total reward for each episode
+y_final = [y[:, -1, 0] for y in y_full]
+mean_y, std_y = zip(
+    *((np.mean(y), np.std(y)) for y in y_final)
+)
+ep_axs[YIELD_indx].bar(labels, mean_y, color=colors)
+ep_axs[YIELD_indx].errorbar(
+    labels, mean_y, std_y, marker=None, ls="none", color="black", capsize=7
+)
 # ep_axs[YIELD_indx].set_yscale("log")
 ep_axs[YIELD_indx].set_ylabel(r"$y_1(N_s)$")
+ep_axs[YIELD_indx].set_ylim([100, 200])
 
 # calculate ecomonmic performance index
 U = [o["U"] for o in data]
@@ -131,21 +166,18 @@ EPI = [
 ]  # converting co2 from mg to kg
 
 # plot economic performance index
-labels=[
-        "I-MPC",
-        "N-MPC",
-        "R-MPC-5",
-        "R-MPC-10",
-        "R-MPC-20",
-        "our",
-        "DDPG"
-    ]
-ep_axs[EPI_indx].boxplot(
-    EPI
-)  # plot the total reward for each episode
-ep_axs[EPI_indx].set_xticks(list(range(1, len(labels)+1)), labels)
+mean_EPI, std_EPI = zip(
+    *((np.mean(epi), np.std(epi)) for epi in EPI)
+)
+ep_axs[EPI_indx].bar(labels, mean_EPI, color=colors)
+ep_axs[EPI_indx].errorbar(
+    labels, mean_EPI, std_EPI, marker=None, ls="none", color="black", capsize=7
+)
+ep_axs[EPI_indx].set_xticks(list(range(len(labels))), labels)
 ep_axs[EPI_indx].set_ylabel(r"$P$")
 
 # save2tikz(plt.gcf())
+
+
 
 plt.show()
