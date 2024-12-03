e423b5726d9e500a18a19c41e3543b366c835965
SamuelMallick <sam.mallick.97@gmail.com>
2024-12-03 11:37:05 +0100

    recommited faster linear solvers

--- mpcs/nominal.py
+++ mpcs/nominal.py
@@ -135,7 +135,7 @@ class NominalMpc(Mpc[cs.SX]):
                 "max_iter": 2000,
                 "print_user_options": "yes",
                 "print_options_documentation": "no",
-                # "linear_solver": "ma57",  # spral
+                "linear_solver": "ma57",  # spral
                 "nlp_scaling_method": "gradient-based",
                 "nlp_scaling_max_gradient": 10,
             },
